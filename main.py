59fe065c655998a51ec9e028436e7ff4a39fb48c
vclark19 <80633789+vclark19@users.noreply.github.com>
2021-06-09 10:29:59 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -61,14 +61,31 @@ class Application(Frame):
         self.canvas = Canvas(self, width=500, height=200)
         self.canvas.pack()
         self.myFont_heading = font.Font(family='Helvetica', size=20)
-        self.canvas.create_text(250, 75, text= "Choose File Directory", font= self.myFont_heading)
+        self.canvas.create_text(250, 25, text= "Choose File Directory", font= self.myFont_heading)
         
         self.browse = Button(self,
                              text="Browse Files", width=15,
                              command=self.explorer_file)
-        self.browse_c = self.canvas.create_window(190, 100,
+        
+        self.browse_c = self.canvas.create_window(190, 50,
                                                   anchor="nw",
                                                   window=self.browse)
+        
+        self.canvas.create_text(245, 100, text="Write New File Name",
+                                font=self.myFont_heading)
+        self.new_entry = StringVar()
+        self.new_entry_name = Entry(self, textvariable=self.new_entry,
+                                    width=30)
+        new_entry_name = self.canvas.create_window(150, 130, 
+                                                   anchor='nw',
+                                                   window=self.new_entry_name)
+        self.rename_b = Button(self,
+                             text="Enter", width=10,
+                             command=self.confirm)
+        
+        self.rename_bb = self.canvas.create_window(200, 155,
+                                                  anchor="nw",
+                                                  window=self.rename_b)
         
     def explorer_file(self):
         
@@ -78,19 +95,34 @@ class Application(Frame):
                                                title="Choose a file.")
 
         self.files = os.listdir(self.filename)
+        # print(self.files)
+        self.a= sorted(self.files, key = self.last_char)
         
+    def confirm(self):
+        answer_txt = 'Do you want to rename all text files in this directory to "' + self.new_entry.get() + "_*.txt" +'" ?'
+        answer = mb.askquestion('Confirm Folder', answer_txt)
+        if answer == 'yes':
+            self.rename()
+        if answer == 'no':
+            pass
+        
+    def last_char(self, x):
+        return(x[-5:3])
+    
+    def rename(self):
         count = 1
-        
-        for i in self.files:
+        for i in self.a:
             if i.endswith(".txt"):
                 name , extension = path.splitext(i)
-                new_name = name + "_" + str(count) + extension
+                new_name = self.new_entry.get() + "_" + str(count) + extension
                 before = os.path.join(self.filename, i)
                 after = os.path.join(self.filename, new_name)
                 os.rename(before, after)
                 count += 1
-            
-        count = 1
+                
+        mb.showinfo("Success!", "Files successfully renamed!")
+      
+       
         
       
        
